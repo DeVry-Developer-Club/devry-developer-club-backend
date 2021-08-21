a2d23df55cefa61906214bd7b61e40b45756cb74
jbraunsmajr <jonbraunsma@gmail.com>
2021-08-21 00:18:34 -0400

    Removed unused class

--- DevryDeveloperClub.Domain/ViewModels/Authentication/AuthenticationResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace DevryDeveloperClub.Domain.ViewModels.Authentication
-{
-    public class AuthenticationResponse
-    {
-        public string Id { get; set; }
-        public string Username { get; set; }
-        public string JwtToken { get; set; }
-    }
-}
